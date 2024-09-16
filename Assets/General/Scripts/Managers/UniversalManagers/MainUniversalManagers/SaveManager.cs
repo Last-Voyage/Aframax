a8a44785b25f439661dc853732d5134dbc3d0e6b
Ryan S. <raswanson@mail.bradley.edu>
2024-09-16 13:21:15 -0500

    SaveSystem

--- Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/SaveManager.cs
+++ Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/SaveManager.cs
@@ -1,17 +1,134 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
 
 public class SaveManager : MainUniversalManagerFramework
 {
-    #region Base Manager
+    //Game Save Data variable MUST viewable in editor for Json, so either public or serialized
+    [SerializeField] private GameSaveData _gameSaveData;
+    private string _path;
+
+    /// <summary>
+    /// TESTING PURPOSES ONLY
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            _gameSaveData.SetTempBool(!_gameSaveData.GetTempBool());
+            SaveText();
+        }
+    }
+
+    /// <summary>
+    /// Sets the path to create the save file
+    /// </summary>
+    private void EstablishPath()
+    {
+        //Checks to see if we're in editor, if so use datapath instead of persistent datapath
+        //Needed so that saving works both in editor and build
+        _path = Application.isEditor ? Application.dataPath : Application.persistentDataPath;
+        //Append /SaveData/ to said path
+        //SaveData is the file is assets containing the save data
+        _path = _path + "/SaveData/";
+        //Check if we're in a build, check if the directory exists, if not
+        if (!Application.isEditor && !Directory.Exists(_path)) 
+        {
+            //Creates the directory
+            Directory.CreateDirectory(_path); 
+        }
+    }
+
+
+    /// <summary>
+    /// Fills the save data with it's initial values when the file is first created as needed
+    /// You could use this to populate a dictionary on start up for example
+    /// For simpler variable types just set them in the Game Save Data class
+    /// </summary>
+    private void StartingValues()
+    {
+        
+    }
+
+    /// <summary>
+    /// Writes all variables in the Game Save Data class into Json
+    /// </summary>
+    public void SaveText()
+    {
+        //Converts the Game Save Data class into a string
+        var convertedJson = JsonConvert.SerializeObject(_gameSaveData);
+        //Saves the string into the text file
+        File.WriteAllText(_path + "Data.json", convertedJson);
+    }
+
+    public void Load()
+    {
+        //Loads all variables in Json into the Game Save Data class
+        if (File.Exists(_path + "Data.json"))
+        {
+            //Converts the text file into a string
+            var json = File.ReadAllText(_path + "Data.json");
+            //Converts the string into the Game Save Data class
+            _gameSaveData = JsonConvert.DeserializeObject<GameSaveData>(json);
+        }
+        else
+        {
+            //Sets the initial values
+            StartingValues();
+            //Saves the initial values
+            SaveText();
+        }
+    }
+
+
+
+    public void ResetSaveData()
+    {
+        //Fully resets all variables in the Game Save Data
+        _gameSaveData = new GameSaveData();
+
+        //Sets the initial values
+        StartingValues();
+
+        //Saves the changes into the text file
+        SaveText();
+    }
+
+    #region BaseManager
     public override void SetupMainManager()
     {
         base.SetupMainManager();
+        EstablishPath();
+        Load();
     }
     #endregion
 
     #region Getters
+    public GameSaveData GetGameSaveData() => _gameSaveData;
+    #endregion
 
+    #region Setters
+
+    #endregion
+
+
+}
+
+[System.Serializable]
+public class GameSaveData
+{
+    public bool tempSaveBool = false;
+
+    #region Getters
+    public bool GetTempBool() => tempSaveBool;
+    #endregion
+
+    #region Setters
+    public void SetTempBool(bool newTemp)
+    {
+        tempSaveBool = newTemp;
+    }
     #endregion
 }
