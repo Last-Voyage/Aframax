ad7aeadec1ea32e40f622f023644caf76f5c7f14
AlecatEnjoyer <Mark25608@yahoo.com>
2024-09-17 09:54:47 -0500

    small edits and some comments

--- Assets/General/Scripts/bulletBehavior.cs
+++ Assets/General/Scripts/bulletBehavior.cs
@@ -3,7 +3,7 @@
 // Author :            Mark Hanson
 // Creation Date :     9/16/2024
 //
-// Brief Description : THIS IS A PLACEHOLDER THIS IS NOTHING BUT A PLACEHOLDER AND IS TO BE DELETED WHEN THE REAL GUN AND BULLET IS IN
+// Brief Description : THIS IS A PLACEHOLDER THIS IS NOTHING BUT A PLACEHOLDER AND IS TO BE DELETED WHEN THE REAL GUN AND BULLET IS IN (CERTAIN ASPECTS ARE TO BE TAKEN FROM IT THOUGH)
 *****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +11,7 @@ using UnityEngine;
 
 public class bulletBehavior : MonoBehaviour
 {
+    //The UI component that serves as feedback of how much damage was taken
     [SerializeField] private GameObject _damCount;
 
     /// <summary>
@@ -23,17 +24,21 @@ public class bulletBehavior : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
+        //If it touches the weak spot the bullet sets the UI count awake and then detaches from it once the correct number is applied to the specific number while despawning
         if (col.gameObject.tag == "Weak Spot")
         {
             _damCount.SetActive(true);
+            //Getting the script from the serialized ui component to change the number
             damageNumBehavior dc = _damCount.GetComponent<damageNumBehavior>();
             dc._damageNumber = 15;
             this.gameObject.transform.DetachChildren();
             Destroy(gameObject ,0.1f);
         }
+        //If it touches the invulnerable spot the bullet sets the UI count awake and then detaches from it once it applies the correct number while despawning
         if (col.gameObject.tag == "Invulnerable Spot")
         {
             _damCount.SetActive(true);
+            //Getting the script from the serialized ui component to change the number
             damageNumBehavior dc = _damCount.GetComponent<damageNumBehavior>();
             dc._damageNumber = 0;
             this.gameObject.transform.DetachChildren();
--- Assets/General/Scripts/damageNumBehavior.cs
+++ Assets/General/Scripts/damageNumBehavior.cs
@@ -12,20 +12,25 @@ using TMPro;
 
 public class damageNumBehavior : MonoBehaviour
 {
+    //The actual text of the game object to be edited 
     [SerializeField] private TextMeshPro _damText;
+    //The number which the text reflects visually
     [SerializeField] public float _damageNumber = 0;
+    //The number that gives the illusion of the number disappearing
     [SerializeField] private float _opacityScaler = 255;
 
     // Update is called once per frame
     void Update()
     {
         _damText.text = _damageNumber.ToString();
-        //slowly disappears
+        //disappears quickly
         _opacityScaler -= 5f;
-        _damText.color = new Color(255f, (255f - (_damageNumber *17f)), (255f - (_damageNumber * 17f)), _opacityScaler);
+        //update the color of the text to match the intesity of which it has been hit(more damage more red) along with opacity going clear
+        _damText.color = new Color(255f/255f, ((255f - (_damageNumber *17f)))/255f, (255f - (_damageNumber * 17f))/255f, _opacityScaler);
         //move Upwards slowly
         transform.position += new Vector3(0, 0.05f, 0);
 
+        //Once text is completely clear destory object
         if (_damText.color.a <= 0)
         {
             StartCoroutine(despawn());
