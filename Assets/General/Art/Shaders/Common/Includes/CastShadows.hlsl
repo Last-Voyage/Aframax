48a80283b3ef0afcaddaf867ee2e55a653e8daad
Miles Rogers <mrogart2003@gmail.com>
2024-09-18 19:35:00 -0500

    Refactor/Finalization
    Refactored/tested the shader, is ready for review.

--- Assets/General/Art/Shaders/Common/Includes/CastShadows.hlsl
+++ Assets/General/Art/Shaders/Common/Includes/CastShadows.hlsl
@@ -1,5 +1,13 @@
-#ifndef CUSTOM_LIGHTING_INCLUDED
-#define CUSTOM_LIGHTING_INCLUDED
+/**************************************************************************
+// File Name :          CastShadows.hlsl
+// Author :             Miles Rogers
+// Creation Date :      9/18/2024
+//
+// Brief Description :  Gathers all shadow data from the Unity World.
+**************************************************************************/
+
+#ifndef CAST_SHADOWS_INCLUDED
+#define CAST_SHADOWS_INCLUDED
 
 void CalculateCustomShadows_float(
     float3 WorldPos,
@@ -7,15 +15,22 @@ void CalculateCustomShadows_float(
     out float ShadowAtten)
 {
 #ifdef SHADERGRAPH_PREVIEW
+    // No need for this information in the
+    // ShaderGraph preview
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
 #if SHADOWS_SCREEN
+    // Gather shadow coordinate info from 
+    // screen-space, if it exists
     float4 clipPos = TransformWorldToHClip(WorldPos);
     float4 shadowCoord = ComputeScreenPos(clipPos);
 #else
+    // Gather world-space shadow coordinate info
     float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
 #endif
+    // Sample shadow info from main light
+    // (DistanceAtten * ShadowAtten = Full Shadow Data)
     Light mainLight = GetMainLight(shadowCoord);
     DistanceAtten = mainLight.distanceAttenuation;
     ShadowAtten = mainLight.shadowAttenuation;
