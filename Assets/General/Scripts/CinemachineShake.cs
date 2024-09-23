3d5454d7ab58b59c0d74b59ecab598be23222b64
troberts2 <troberts2@mail.bradley.edu>
2024-09-22 19:50:20 -0500

    some script fixes

--- Assets/General/Scripts/CinemachineShake.cs
+++ Assets/General/Scripts/CinemachineShake.cs
@@ -1,3 +1,10 @@
+/*****************************************************************************
+// File Name :         CinemachineShake.cs
+// Author :            Tommy Roberts
+// Creation Date :     9/22/2024
+//
+// Brief Description : Shakes the camera ;)
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +15,18 @@ public class CinemachineShake : MonoBehaviour
     public static CinemachineShake Instance { get; private set; }
     private CinemachineVirtualCamera _cinemachineVirtualCam;
     private float _shakeTimer;
+    /// <summary>
+    /// creates an instance of the CinemachineShake object so you can call a shake anywhere you wwant.
+    /// </summary>
     private void Awake() {
         Instance = this;
         _cinemachineVirtualCam = GetComponent<CinemachineVirtualCamera>();
     }
+    /// <summary>
+    /// times the shake
+    /// </summary>
+    /// <param name="intensity"></param>
+    /// <param name="time"></param>
     public void ShakeCamera(float intensity, float time){
         CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = 
             _cinemachineVirtualCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
@@ -19,6 +34,9 @@ public class CinemachineShake : MonoBehaviour
         cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
         _shakeTimer = time;
     }
+    /// <summary>
+    /// times the shake
+    /// </summary>
     private void Update() {
         if(_shakeTimer > 0){
             _shakeTimer -= Time.deltaTime;
