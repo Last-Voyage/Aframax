6e41ed8a8284318ab32b51b3bf1399713b89cda0
Ryan S. <raswanson@mail.bradley.edu>
2024-09-28 11:00:33 -0500

    PlayerHarpoonCombined

--- Assets/FunctionalTeams/Player/Input/PlayerInputMap.cs
+++ Assets/FunctionalTeams/Player/Input/PlayerInputMap.cs
@@ -55,9 +55,27 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": false
                 },
                 {
-                    ""name"": ""HarpoonButton"",
+                    ""name"": ""FireHarpoon"",
                     ""type"": ""Button"",
                     ""id"": ""aa80d4fd-ee8d-4409-b4fc-02c693475a30"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""FocusHarpoon"",
+                    ""type"": ""Button"",
+                    ""id"": ""b8545e07-0d14-4c11-96b3-0a45742a7a07"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""ReelHarpoon"",
+                    ""type"": ""Button"",
+                    ""id"": ""46123ba1-202e-49de-a560-ebd47b8d6d5c"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """",
@@ -149,7 +167,29 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""HarpoonButton"",
+                    ""action"": ""FireHarpoon"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a56680af-279a-4e05-80e3-21fe7f52c577"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""FocusHarpoon"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fe6eab22-65f6-4bcf-b7c0-fa1f58688e0a"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ReelHarpoon"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -163,7 +203,9 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
         m_Player_MouseX = m_Player.FindAction("MouseX", throwIfNotFound: true);
         m_Player_MouseY = m_Player.FindAction("MouseY", throwIfNotFound: true);
-        m_Player_HarpoonButton = m_Player.FindAction("HarpoonButton", throwIfNotFound: true);
+        m_Player_FireHarpoon = m_Player.FindAction("FireHarpoon", throwIfNotFound: true);
+        m_Player_FocusHarpoon = m_Player.FindAction("FocusHarpoon", throwIfNotFound: true);
+        m_Player_ReelHarpoon = m_Player.FindAction("ReelHarpoon", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -228,7 +270,9 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Movement;
     private readonly InputAction m_Player_MouseX;
     private readonly InputAction m_Player_MouseY;
-    private readonly InputAction m_Player_HarpoonButton;
+    private readonly InputAction m_Player_FireHarpoon;
+    private readonly InputAction m_Player_FocusHarpoon;
+    private readonly InputAction m_Player_ReelHarpoon;
     public struct PlayerActions
     {
         private @PlayerInputMap m_Wrapper;
@@ -236,7 +280,9 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
         public InputAction @MouseX => m_Wrapper.m_Player_MouseX;
         public InputAction @MouseY => m_Wrapper.m_Player_MouseY;
-        public InputAction @HarpoonButton => m_Wrapper.m_Player_HarpoonButton;
+        public InputAction @FireHarpoon => m_Wrapper.m_Player_FireHarpoon;
+        public InputAction @FocusHarpoon => m_Wrapper.m_Player_FocusHarpoon;
+        public InputAction @ReelHarpoon => m_Wrapper.m_Player_ReelHarpoon;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -255,9 +301,15 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
             @MouseY.started += instance.OnMouseY;
             @MouseY.performed += instance.OnMouseY;
             @MouseY.canceled += instance.OnMouseY;
-            @HarpoonButton.started += instance.OnHarpoonButton;
-            @HarpoonButton.performed += instance.OnHarpoonButton;
-            @HarpoonButton.canceled += instance.OnHarpoonButton;
+            @FireHarpoon.started += instance.OnFireHarpoon;
+            @FireHarpoon.performed += instance.OnFireHarpoon;
+            @FireHarpoon.canceled += instance.OnFireHarpoon;
+            @FocusHarpoon.started += instance.OnFocusHarpoon;
+            @FocusHarpoon.performed += instance.OnFocusHarpoon;
+            @FocusHarpoon.canceled += instance.OnFocusHarpoon;
+            @ReelHarpoon.started += instance.OnReelHarpoon;
+            @ReelHarpoon.performed += instance.OnReelHarpoon;
+            @ReelHarpoon.canceled += instance.OnReelHarpoon;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -271,9 +323,15 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
             @MouseY.started -= instance.OnMouseY;
             @MouseY.performed -= instance.OnMouseY;
             @MouseY.canceled -= instance.OnMouseY;
-            @HarpoonButton.started -= instance.OnHarpoonButton;
-            @HarpoonButton.performed -= instance.OnHarpoonButton;
-            @HarpoonButton.canceled -= instance.OnHarpoonButton;
+            @FireHarpoon.started -= instance.OnFireHarpoon;
+            @FireHarpoon.performed -= instance.OnFireHarpoon;
+            @FireHarpoon.canceled -= instance.OnFireHarpoon;
+            @FocusHarpoon.started -= instance.OnFocusHarpoon;
+            @FocusHarpoon.performed -= instance.OnFocusHarpoon;
+            @FocusHarpoon.canceled -= instance.OnFocusHarpoon;
+            @ReelHarpoon.started -= instance.OnReelHarpoon;
+            @ReelHarpoon.performed -= instance.OnReelHarpoon;
+            @ReelHarpoon.canceled -= instance.OnReelHarpoon;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -296,6 +354,8 @@ public partial class @PlayerInputMap: IInputActionCollection2, IDisposable
         void OnMovement(InputAction.CallbackContext context);
         void OnMouseX(InputAction.CallbackContext context);
         void OnMouseY(InputAction.CallbackContext context);
-        void OnHarpoonButton(InputAction.CallbackContext context);
+        void OnFireHarpoon(InputAction.CallbackContext context);
+        void OnFocusHarpoon(InputAction.CallbackContext context);
+        void OnReelHarpoon(InputAction.CallbackContext context);
     }
 }
