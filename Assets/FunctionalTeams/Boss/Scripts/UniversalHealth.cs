727d18de5f62da7a0e2966891a5c3664c0ea3514
AlecatEnjoyer <Mark25608@yahoo.com>
2024-09-29 11:49:24 -0500

    universal health script edits

--- Assets/FunctionalTeams/Boss/Scripts/UniversalHealth.cs
+++ Assets/FunctionalTeams/Boss/Scripts/UniversalHealth.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class UniversalHealth : UniversalHealthSystem
 {
-    [Tooltip("Used to Determine health numbers and status of character")]
+    [Header("Health Description")]
+    [Tooltip("Health at the beginning of game")]
     [SerializeField] private float _maxHealth;
-    [SerializeField] private float _currentHealth;
+    [Tooltip("Determines if player or not")]
     [SerializeField] private bool _isPlayer;
+
     private Health health;
     void Start()
     {
-        health = new Health(_maxHealth, _currentHealth, _isPlayer);
+        health = new Health(_maxHealth, _maxHealth, _isPlayer);
         Debug.Log(health.MaxHealth);
         Debug.Log(health.CurrentHealth);
         Debug.Log(health.IsPlayer);
@@ -19,6 +21,26 @@ public class UniversalHealth : UniversalHealthSystem
     // Update is called once per frame
     void Update()
     {
-
+        if(health.WaveClear == true)
+        {
+            health.CurrentHealth = health.MaxHealth;
+            health.WaveClear = false;
+        }
+        if(health.CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if(health.CurrentHealth > health.MaxHealth)
+        {
+            health.CurrentHealth = health.MaxHealth;
+        }
+    }
+    public void HealthDecrease(float _damage)
+    {
+        health.CurrentHealth -= _damage;
+    }
+    public void HealthIncrease(float _heal)
+    {
+        health.CurrentHealth += _heal;
     }
 }
