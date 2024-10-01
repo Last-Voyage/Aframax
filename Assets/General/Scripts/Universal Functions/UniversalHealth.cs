4f1f60f7347764f6b3761614e05d9e467154f7e3
AlecatEnjoyer <Mark25608@yahoo.com>
2024-09-30 23:24:25 -0500

    universal health with comments

---
+++ Assets/General/Scripts/Universal Functions/UniversalHealth.cs
@@ -0,0 +1,61 @@
+/*****************************************************************************
+// File Name :         UniversalHealth.cs
+// Author :            Mark Hanson
+// Creation Date :     9/27/2024
+//
+// Brief Description : The functational application of the health using editable serialize fields 
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalHealth : UniversalHealthSystem
+{
+    [Header("Health Description")]
+    [Tooltip("Health at the beginning of game")]
+    [SerializeField] private float _maxHealth;
+    [Tooltip("Determines if player or not")]
+    [SerializeField] private bool _isPlayer;
+    //The health assets and variables that will be used derive from here
+    private Health health;
+    void Start()
+    {
+        health = new Health(_maxHealth, _maxHealth, _isPlayer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //When a wave clears health goes back to max
+        if (health.WaveClear == true && health.IsPlayer == true)
+        {
+            health.CurrentHealth = health.MaxHealth;
+            health.WaveClear = false;
+        }
+        // If the health goes all the way down to 0 the game object dies
+        if (health.CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //If while healing the amount of health exceeds the max amount of health return it back to the max instead of leaving higher
+        if (health.CurrentHealth > health.MaxHealth)
+        {
+            health.CurrentHealth = health.MaxHealth;
+        }
+    }
+    /// <summary>
+    /// A way for health to be damaged
+    /// </summary>
+    /// <param name="_damage"></param>
+    public void HealthDecrease(float _damage)
+    {
+        health.CurrentHealth -= _damage;
+    }
+    /// <summary>
+    /// A way for the health to be healed
+    /// </summary>
+    /// <param name="_heal"></param>
+    public void HealthIncrease(float _heal)
+    {
+        health.CurrentHealth += _heal;
+    }
+}
