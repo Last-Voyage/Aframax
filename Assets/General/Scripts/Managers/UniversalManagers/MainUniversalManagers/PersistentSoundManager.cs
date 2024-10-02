78fdfaec9b2de0e690d5121231c36a2faee3b3c6
Andrea <andreaswihartdecoster@gmail.com>
2024-10-01 20:45:59 -0500

    Simple One Shot SFX Functionality

---
+++ Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/PersistentSoundManager.cs
@@ -0,0 +1,27 @@
+/******************************************************************************
+// File Name:       PersistentSoundManager.cs
+// Author:          Andrea Swihart-DeCoster
+// Creation Date:   October 1st, 2024
+//
+// Description:     Manages any sound that player persistently throughout the
+                    game. e.g music, background audio, etc
+******************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentSoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
