53f597166977c744ac95865c641bce2e7f3b3e8b
AlecatEnjoyer <Mark25608@yahoo.com>
2024-10-14 17:50:11 -0500

    xml comments

--- Assets/General/Scripts/Universal Functions/BaseHealth.cs
+++ Assets/General/Scripts/Universal Functions/BaseHealth.cs
@@ -9,14 +9,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-//The class the will be referred from to call for certain functions
+/// <summary>
+/// The class the will be referred from to call for certain functions
+/// </summary>
 public class BaseHealth : MonoBehaviour
 {
     protected int _currentHealth;
     protected int _maxHealth;
     //Event system used to be called outside of script
     private UnityEvent<BaseHealth> _deathEvent = new ();
-    //Function for decreasing health 
+    /// <summary>
+    /// Function for decreasing health 
+    /// </summary>
+    /// <param name="damage"></param>
     protected virtual void HealthDecrease(int damage)
     {
         _currentHealth -= damage;
@@ -26,6 +31,10 @@ public class BaseHealth : MonoBehaviour
             InvokeDeathEvent();
         }
     }
+    /// <summary>
+    /// function of health increasing
+    /// </summary>
+    /// <param name="heal"></param>
     protected virtual void HealthIncrease(int heal)
     {
         _currentHealth += heal;
@@ -36,6 +45,9 @@ public class BaseHealth : MonoBehaviour
         }
     }
     #region Events
+    /// <summary>
+    /// Death event to kill gameObject from the object
+    /// </summary>
     private void InvokeDeathEvent()
     {
         _deathEvent?.Invoke(this);
