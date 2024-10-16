1a6fad0c5c4444529558363fb5fadd583cc47ac7
Ryan S. <raswanson@mail.bradley.edu>
2024-10-16 13:40:23 -0500

    Revert with tag removed
    This reverts commit c9d3fca50ddb8c26082321cdc9c3a8bab68cbc8c.

--- Assets/FunctionalTeams/Boss/Scripts/bulletBehavior.cs
+++ Assets/FunctionalTeams/Boss/Scripts/bulletBehavior.cs
@@ -19,9 +19,6 @@ public class BulletBehavior : MonoBehaviour
     //Adjustible number for designers outside of code (default damage is 15)
     [SerializeField] private int _actualDamage = 15;
 
-    [Space]
-    [SerializeField] private bool _damageNumberToggle;
-
     void Start()
     {
         //StartCoroutine(delayDespawn());
@@ -44,26 +41,15 @@ public class BulletBehavior : MonoBehaviour
     /// <param name="col"></param>
     void OnTriggerEnter(Collider col)
     {
-        DamageNumBehavior dc = null ;
-        TextMeshPro damageText = null;
-
-        if (_damageNumberToggle)
-        {
-            Instantiate(_damageCount, this.transform);
-            _exactCounter = this.transform.GetChild(0).gameObject;
-            dc = _exactCounter.GetComponent<DamageNumBehavior>();
-            damageText = _exactCounter.GetComponent<TextMeshPro>();
-        }
-        
+        Instantiate(_damageCount, this.transform);
+        _exactCounter = this.transform.GetChild(0).gameObject;
+        DamageNumBehavior dc = _exactCounter.GetComponent<DamageNumBehavior>();
+        TextMeshPro _damageText = _exactCounter.GetComponent<TextMeshPro>();
         //If it touches the weak spot the bullet sets the UI count awake and then detaches from it once the correct number is applied to the specific number while despawning
         if (col.gameObject.tag == "Weak Spot")
         {
-            if (_damageNumberToggle)
-            {
-                dc.DamageNumber = _actualDamage;
-                damageText.text = _actualDamage.ToString();
-            }
-                
+            dc.DamageNumber = _actualDamage;
+            _damageText.text = _actualDamage.ToString();
             this.gameObject.transform.DetachChildren();
             Destroy(col.gameObject);
             //destroy after taking out weak spot
@@ -72,13 +58,9 @@ public class BulletBehavior : MonoBehaviour
         //If it touches the invulnerable spot the bullet sets the UI count awake and then detaches from it once it applies the correct number while despawning
         else if (col.gameObject.tag == "Invulnerable Spot")
         {
-            if (_damageNumberToggle)
-            {
-                dc.DamageNumber = 0;
-                this.gameObject.transform.DetachChildren();
-                //Destroy(gameObject);
-            }
-
+            dc.DamageNumber = 0;
+            this.gameObject.transform.DetachChildren();
+            //Destroy(gameObject);
         }
     }
 }
