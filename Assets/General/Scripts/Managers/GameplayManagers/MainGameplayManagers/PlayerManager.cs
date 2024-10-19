54440216996435d609a51c84077090eecbebfbc1
Ryan S. <raswanson@mail.bradley.edu>
2024-10-19 13:59:33 -0500

    RopeAndReelingRemoved
    Rest in peace harpoon rope. I hope you are in a better place now. In all seriousness, deleted the rope and rope spring functionality. Removed the functionality for the harpoon to retract. Instead switched to a reload. This includes changes to the player manager to accordingly. Integrated object pooling into the harpoon projectile.

--- Assets/General/Scripts/Managers/GameplayManagers/MainGameplayManagers/PlayerManager.cs
+++ Assets/General/Scripts/Managers/GameplayManagers/MainGameplayManagers/PlayerManager.cs
@@ -25,54 +25,33 @@ public class PlayerManager : MainGameplayManagerFramework
     private static UnityEvent<bool> _onMovementToggled = new();
 
     //When the projectile is fired
-    private static UnityEvent _harpoonFiredStartEvent = new();
-    //When the projectile hits something or reaches its max distance
-    private static UnityEvent _harpoonFiredEndEvent = new();
-    //When the player presses the reel button while actively reeling
-    private static UnityEvent _harpoonRetractStartEvent = new();
-    //When the player releases the reel button while actively reeling
-    private static UnityEvent _harpoonRetractStoppedEvent = new();
-    //When the harpoon is fully reeled in
-    private static UnityEvent _harpoonFullyReeledEvent = new();
+    private static UnityEvent _onHarpoonFiredEvent = new();
     //When the harpoon is reloaded
-    private static UnityEvent _harpoonReloadedEvent = new();
+    private static UnityEvent _onHarpoonReloadedEvent = new();
 
     //When the player starts focusing
-    private static UnityEvent _harpoonFocusStartEvent = new();
+    private static UnityEvent _onHarpoonFocusStartEvent = new();
     //When the player is at max focus
-    private static UnityEvent _harpoonFocusMaxEvent = new();
+    private static UnityEvent _onHarpoonFocusMaxEvent = new();
     //When the player is no longer focused
-    private static UnityEvent _harpoonFocusEndEvent = new();
+    private static UnityEvent _onHarpoonFocusEndEvent = new();
 
-    private static UnityEvent _enemyOverCrosshairStartEvent = new();
-    private static UnityEvent _enemyOverCrosshairEndEvent = new();
-
-    //When the player takes damage
-    private static UnityEvent<float> _onPlayerDamageEvent = new();
-    //When the player receives healing
-    private static UnityEvent<float> _onPlayerHealEvent = new();
-    //When the player health changes for any reason
-    private static UnityEvent<float, float> _onHealthChange = new();
-    //When the player dies
-    private static UnityEvent _onPlayerDeath = new();
+    private static UnityEvent _onEnemyOverCrosshairStartEvent = new();
+    private static UnityEvent _onEnemyOverCrosshairEndEvent = new();
 
     #region Base Manager
-    
     public override void SetupInstance()
     {
         base.SetupInstance();
     }
-    
     public override void SetupMainManager()
     {
         base.SetupMainManager();
         Instance = this;
     }
-    
     #endregion
 
     #region Events
-    
     /// <summary>
     /// Invokes the _onMovementToggled event with the input bool
     /// </summary>
@@ -85,147 +64,69 @@ public class PlayerManager : MainGameplayManagerFramework
     /// <summary>
     /// Invokes event for when the harpoon projectile is fired
     /// </summary>
-    public void InvokeHarpoonFiredStartEvent()
+    public void InvokeOnHarpoonFiredEvent()
     {
-        _harpoonFiredStartEvent?.Invoke();
+        _onHarpoonFiredEvent?.Invoke();
     }
 
-    /// <summary>
-    /// Invokes event for when the harpoon has collided with an object or reached max range
-    /// </summary>
-    public void InvokeHarpoonFiredEndEvent()
+    public void InvokeOnHarpoonReloadedEvent()
     {
-        _harpoonFiredEndEvent?.Invoke();
-    }
-
-    /// <summary>
-    /// Invokes event for when the player starts retracting the harpoon projectile
-    /// </summary>
-    public void InvokeHarpoonRetractStartEvent()
-    {
-        _harpoonRetractStartEvent?.Invoke();
-    }
-
-    /// <summary>
-    /// Invokes event for when the player releases the retract button while reeling
-    /// </summary>
-    public void InvokeHarpoonRetractStoppedEvent()
-    {
-        _harpoonRetractStoppedEvent?.Invoke();
-    }
-
-    public void InvokeHarpoonFullyReeledEvent()
-    {
-        _harpoonFullyReeledEvent?.Invoke();
+        _onHarpoonReloadedEvent?.Invoke();
     }
 
     /// <summary>
     /// Invokes the harpoon focus start event
     /// </summary>
-    public void InvokeHarpoonFocusStartEvent()
+    public void InvokeOnHarpoonFocusStartEvent()
     {
-        _harpoonFocusStartEvent?.Invoke();
+        _onHarpoonFocusStartEvent?.Invoke();
     }
-    
     /// <summary>
     /// Invokes the harpoon focus max event
     /// </summary>
-    public void InvokeHarpoonFocusMaxEvent()
+    public void InvokeOnHarpoonFocusMaxEvent()
     {
-        _harpoonFocusMaxEvent?.Invoke();
+        _onHarpoonFocusMaxEvent?.Invoke();
     }
-    
     /// <summary>
     /// Invokes the harpoon focus end event
     /// </summary>
-    public void InvokeHarpoonFocusEndEvent()
+    public void InvokeOnHarpoonFocusEndEvent()
     {
-        _harpoonFocusEndEvent?.Invoke();
+        _onHarpoonFocusEndEvent?.Invoke();
     }
 
     /// <summary>
     /// Invokes the crosshair over enemy start event
     /// </summary>
-    public void InvokeCrosshairOverEnemyStartEvent()
+    public void InvokeOnCrosshairOverEnemyStartEvent()
     {
-        _enemyOverCrosshairStartEvent?.Invoke();
+        _onEnemyOverCrosshairStartEvent?.Invoke();
     }
 
     /// <summary>
     /// Invokes the crosshair over enemy end event
     /// </summary>
-    public void InvokeCrosshairOverEnemyEndEvent()
+    public void InvokeOnCrosshairOverEnemyEndEvent()
     {
-        _enemyOverCrosshairEndEvent?.Invoke();
+        _onEnemyOverCrosshairEndEvent?.Invoke();
     }
-
-    /// <summary>
-    /// Invokes when the player receives damage
-    /// </summary>
-    /// <param name="damageTaken">The damage received</param>
-    public void InvokePlayerDamagedEvent(float damageTaken)
-    {
-        _onPlayerDamageEvent?.Invoke(damageTaken);
-    }
-
-    /// <summary>
-    /// Invokes when the player receives healing
-    /// </summary>
-    /// <param name="healTaken">The healing received</param>
-    public void InvokePlayerHealEvent(float healTaken)
-    {
-        _onPlayerHealEvent?.Invoke(healTaken);
-    }
-
-    /// <summary>
-    /// Invokes when the player health changes by any means
-    /// </summary>
-    /// <param name="percentHealth">The current percent health</param>
-    /// <param name="currentHealth">The current health amount</param>
-    public void InvokePlayerHealthChangeEvent(float percentHealth, float currentHealth)
-    {
-        _onHealthChange?.Invoke(percentHealth, currentHealth);
-    }
-
-    /// <summary>
-    /// Invokes when the player dies
-    /// </summary>
-    public void InvokeOnPlayerDeath()
-    {
-        _onPlayerDeath?.Invoke();
-
-        //when you die it reloads the current scene through the scene loading manager
-        //Will be changed later to have ui pop up button to do this I would imagine
-        SceneLoadingManager.Instance.DeathReloadCurrentScene();
-    }
-    
     #endregion
 
     #region Getters
-    
     /// <summary>
     /// Getter for the _onMovementToggled event
     /// </summary>
-    public UnityEvent<bool> GetMovementToggleEvent() => _onMovementToggled;
+    public UnityEvent<bool> GetOnMovementToggleEvent() => _onMovementToggled;
 
-    public UnityEvent GetHarpoonFiredStartEvent() => _harpoonFiredStartEvent;
-    public UnityEvent GetHarpoonFiredEndEvent() => _harpoonFiredEndEvent;
-    public UnityEvent GetHarpoonRetractStartEvent() => _harpoonRetractStartEvent;
-    public UnityEvent GetHarpoonRetractStoppedEvent() => _harpoonRetractStoppedEvent;
-    public UnityEvent GetHarpoonFullyReeledEvent() => _harpoonFullyReeledEvent;
-    public UnityEvent GetHarpoonReloadedEvent() => _harpoonReloadedEvent;
+    public UnityEvent GetOnHarpoonFiredEvent() => _onHarpoonFiredEvent;
+    public UnityEvent GetOnHarpoonReloadedEvent() => _onHarpoonReloadedEvent;
 
-    public UnityEvent GetHarpoonFocusStartEvent() => _harpoonFocusStartEvent;
-    public UnityEvent GetHarpoonFocusMaxEvent() => _harpoonFocusMaxEvent;
-    public UnityEvent GetHarpoonFocusEndEvent() => _harpoonFocusEndEvent;
+    public UnityEvent GetOnHarpoonFocusStartEvent() => _onHarpoonFocusStartEvent;
+    public UnityEvent GetOnHarpoonFocusMaxEvent() => _onHarpoonFocusMaxEvent;
+    public UnityEvent GetOnHarpoonFocusEndEvent() => _onHarpoonFocusEndEvent;
 
-    public UnityEvent GetEnemyOverCrosshairStartEvent() => _enemyOverCrosshairStartEvent;
-    public UnityEvent GetEnemyOverCrosshairEndEvent() => _enemyOverCrosshairEndEvent;
-
-    public UnityEvent<float> GetOnPlayerDamageEvent() => _onPlayerDamageEvent;
-    public UnityEvent<float> GetOnPlayerHealEvent() => _onPlayerHealEvent;
-    public UnityEvent<float, float> GetOnPlayerHealthChangeEvent() => _onHealthChange;
-    public UnityEvent GetOnPlayerDeath() => _onPlayerDeath;
-    
+    public UnityEvent GetOnEnemyOverCrosshairStartEvent() => _onEnemyOverCrosshairStartEvent;
+    public UnityEvent GetOnEnemyOverCrosshairEndEvent() => _onEnemyOverCrosshairEndEvent;
     #endregion
 }
