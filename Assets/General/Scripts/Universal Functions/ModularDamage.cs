614c8b442a78d4a8bf81e145c066856b12bec199
Andrea Swihart <100057381+ASLH68@users.noreply.github.com>
2024-10-20 17:55:52 -0500

    Updated Spacing in ModularDamage.cs

--- Assets/General/Scripts/Universal Functions/ModularDamage.cs
+++ Assets/General/Scripts/Universal Functions/ModularDamage.cs
@@ -9,6 +9,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 /// <summary>
 /// A list of variables and functions that can be attached to other scripts
 /// </summary>
@@ -16,13 +17,16 @@ public interface ModularDamage
 {
     //The ending damage value used to be passed on to health
     void ApplyDamage();
+    
     //These variables even though currently private will be public hence why they are PascalCase
     [Tooltip("Base damage value")]
     float DamageAmount { get;}
+    
     //When in attack mode it will give actual damage
     //attack mode would be when a move like the tongue is visually swinging or agressing but for the player
     //it is when your harpon would just be shot out ideally not used when reeling back harpoon
     bool CanApplyDamage { get;}
+    
     //event will mostly be used to listen to the universal health variables
     UnityEvent<float> DamageEvent { get; }
 }
