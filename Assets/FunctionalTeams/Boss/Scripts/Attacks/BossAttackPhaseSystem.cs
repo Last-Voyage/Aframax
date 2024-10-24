167e4de5a49f468aa72f4a0d4be8a32dd4476736
AlecatEnjoyer <Mark25608@yahoo.com>
2024-10-24 11:37:14 -0500

    new stuff

--- Assets/FunctionalTeams/Boss/Scripts/Attacks/BossAttackPhaseSystem.cs
+++ Assets/FunctionalTeams/Boss/Scripts/Attacks/BossAttackPhaseSystem.cs
@@ -1,3 +1,10 @@
+/*****************************************************************************
+// File Name :         BaseHealth.cs
+// Author :            Mark Hanson
+// Creation Date :     10/22/2024
+//
+// Brief Description : The system to manage what phase the boss is on and also switch between them along with which attack comes out
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +12,14 @@ using UnityEngine.Events;
 
 public class BossAttackPhaseSystem : MonoBehaviour
 {
+    //Should hold a list of numbers which are being swapped into the current max attacks
+    [Tooltip("How many attacks do you want per phase")]
+    protected private int[] _attacksPerPhase;
+
+    protected private int _currentMaxAttacks;
+
+    protected private int _attackCounter = 0;
+
     //The current phase it is on
     protected private int _phaseCounter = 0;
     //A switch bool that works in tandem with the phase counter
@@ -20,9 +35,25 @@ public class BossAttackPhaseSystem : MonoBehaviour
         _isPhaseOver = false;
         _attackCollection[_phaseCounter].SetActive(true);
         InvokePhaseBegin();
+        _currentMaxAttacks = _attacksPerPhase[_phaseCounter];
     }
 
-
+    private IEnumerator AttackManagement()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            if(_isPhaseOver )
+            {
+                for (int i = 0; i <= _currentMaxAttacks; ++i)
+                {
+                    _attackCollection[_attackCounter].SetActive(true);
+                    _attackCounter++;
+                }
+                _isPhaseOver = false;
+            }
+        }
+    }
 
     /// <summary>
     /// Phase Management works as a way to listen to attack events and elaborate on what phase it should be on
@@ -37,11 +68,9 @@ public class BossAttackPhaseSystem : MonoBehaviour
             if(_isPhaseOver)
             {
                 InvokePhaseEnd();
-                _attackCollection[_phaseCounter].SetActive(false);
                 _phaseCounter++;
-                _attackCollection[_phaseCounter].SetActive(true);
+                _currentMaxAttacks = _attacksPerPhase[_phaseCounter];
                 InvokePhaseBegin();
-                _isPhaseOver = false;
             }
             if(_phaseCounter > _attackCollection.Length)
             {
