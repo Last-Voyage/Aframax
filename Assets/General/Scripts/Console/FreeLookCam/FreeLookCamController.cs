b0e9d954a883a99130c139b57c86a7c293eadd1b
Nabil Tagba <ntagba@mail.bradley.edu>
2024-11-05 21:43:50 -0600

    The free look
    The free look cam works, i Just have to spawn it when the player toggles free cam mode and turn the old virtual cam off and vice versa when toggled off

--- Assets/General/Scripts/Console/FreeLookCam/FreeLookCamController.cs
+++ Assets/General/Scripts/Console/FreeLookCam/FreeLookCamController.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class FreeLookCamController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputMap _playerInput;
+
+    private InputAction _movementInput;
+    private Rigidbody _rb;
+
+    [Header("Settings")]
+    [SerializeField] private float _speed;
+    [SerializeField] private float _lookSpeed;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody _body;
+    [SerializeField] private Transform _cam;
+
+    private void Awake()
     {
+        _playerInput = new PlayerInputMap();
+        _playerInput.Enable();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _movementInput = _playerInput.DebugConsole.FreeCamMoveRight;
+        _playerInput.DebugConsole.FreeCamMoveRight.performed += ctx => Move(ctx.ReadValue<Vector3>(), _body);
+
+    }
+
+
+    private void Move(Vector3 value, Rigidbody body)
+    {
+        Vector3 dir = (_cam.right * value.x) + (_cam.up * value.y) +  (_cam.forward * value.z);
+        body.velocity = dir.normalized * _speed * Time.deltaTime;// dir * speed * delta time
+    }
+
+    
+
+    private void OnDisable()
+    {
+        _playerInput.DebugConsole.FreeCamMoveRight.performed -= ctx => Move(ctx.ReadValue<Vector3>(), _body);
+        _playerInput.Disable();
+
     }
 }
