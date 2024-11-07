e294981b77fd1a6f65f3cc5ba87a5d61a1235686
AlecatEnjoyer <Mark25608@yahoo.com>
2024-11-07 11:45:17 -0600

    Like and subscribe version of map shifting

--- Assets/General/Scripts/BoatLayout/MapShifting.cs
+++ Assets/General/Scripts/BoatLayout/MapShifting.cs
@@ -1,28 +1,85 @@
+/*****************************************************************************
+// File Name :         MapShifting.cs
+// Author :            Mark Hanson
+// Creation Date :     11/7/2024
+//
+// Brief Description : The array of different boat layouts that are accessible in other scripts
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MapShifting : MonoBehaviour
 {
+    //A way of adding layouts into the scene outside of this script
+    [Tooltip("Add all layouts of boat inside of here")]
     [SerializeField]private protected GameObject[] _listOfLayouts;
     private protected int _currentLayout = 0;
-    // Start is called before the first frame update
+    //For calling outside of script into another script
+    private UnityEvent _onMapShifting = new();
+    private UnityEvent _onMapReset = new();
+    /// <summary>
+    /// Allow for first layout to turn on if not on already
+    /// </summary>
     void Start()
     {
         _listOfLayouts[_currentLayout].SetActive(true);
+        Subscribe();
     }
-
+    /// <summary>
+    /// Turns off current layout of boat and goes to the next one
+    /// </summary>
     private protected void LayoutSwapForward()
     {
         _listOfLayouts[_currentLayout].SetActive(false);
         ++_currentLayout;
         _listOfLayouts[_currentLayout].SetActive(true);
     }
-
-    private protected void LayoutSwapBackward()
+    /// <summary>
+    /// Turns off current layout of boat and goes all the way back to the
+    /// Original
+    /// </summary>
+    private protected void LayoutSwapToOriginal()
     {
         _listOfLayouts[_currentLayout].SetActive(false);
-        --_currentLayout;
+        _currentLayout = 0;
         _listOfLayouts[_currentLayout].SetActive(true);
     }
+    /// <summary>
+    /// Attach functionality on to the events 
+    /// </summary>
+    protected virtual void Subscribe()
+    {
+        _onMapReset.AddListener(LayoutSwapToOriginal);
+        _onMapShifting.AddListener(LayoutSwapForward);
+    }
+    /// <summary>
+    /// Detach functionality away from the events
+    /// </summary>
+    protected virtual void Unsubscribe()
+    {
+        _onMapReset.RemoveListener(LayoutSwapToOriginal);
+        _onMapShifting.RemoveListener(LayoutSwapForward);
+    }
+    #region Events
+/// <summary>
+/// For map shifting to be used outside this script
+/// </summary>
+    private void InvokeOnMapShifting()
+    {
+        _onMapShifting?.Invoke();
+    }
+/// <summary>
+///  For map resetting to be used outside this script
+/// </summary>
+    private void InvokeOnMapReset()
+    {
+        _onMapReset?.Invoke();
+    }
+    #endregion
+    #region Getters
+    public UnityEvent OnMapShifting => _onMapShifting;
+    public UnityEvent OnMapReset => _onMapReset;
+    #endregion
 }
