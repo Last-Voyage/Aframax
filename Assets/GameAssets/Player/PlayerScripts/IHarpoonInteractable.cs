3aef1cbf009c6a81e90fd311cf912241050ad0b6
Alex Kalscheur <akalscheur@mail.bradley.edu>
2024-11-08 15:13:48 -0600

    Renamed interfaces
    Adjusted based on code standards and other PR feedback

--- Assets/GameAssets/Player/PlayerScripts/IHarpoonInteractable.cs
+++ Assets/GameAssets/Player/PlayerScripts/IHarpoonInteractable.cs
@@ -1,22 +1,18 @@
 /**********************************************************************************************************************
-// File Name :         PlayerInteractableInterface.cs
+// File Name :         IPlayerInteractable.cs
 // Author :            Alex Kalscheur
 // Creation Date :     11/7/2024
 //
-// Brief Description : Interface for implementing interactable system with player
+// Brief Description : Interface for implementing interactable system with harpoon
 **********************************************************************************************************************/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// Interface for implementing interactable system with player
 /// </summary>
-public interface PlayerInteractableInterface
+public interface IHarpoonInteractable
 {
     /// <summary>
-    /// Will be called when interacted with by the player
+    /// Will be called when the object is hit by a harpoon
     /// </summary>
-    public void OnInteractedByPlayer();
+    abstract void OnHitByHarpoon();
 }
--- Assets/GameAssets/Player/PlayerScripts/IPlayerInteractable.cs
+++ Assets/GameAssets/Player/PlayerScripts/IPlayerInteractable.cs
@@ -1,22 +1,18 @@
 /**********************************************************************************************************************
-// File Name :         PlayerInteractableInterface.cs
+// File Name :         IPlayerInteractable.cs
 // Author :            Alex Kalscheur
 // Creation Date :     11/7/2024
 //
-// Brief Description : Interface for implementing interactable system with harpoon
+// Brief Description : Interface for implementing interactable system with player
 **********************************************************************************************************************/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// Interface for implementing interactable system with player
 /// </summary>
-public interface HarpoonInteractableInterface
+public interface IPlayerInteractable
 {
     /// <summary>
-    /// Will be called when the object is hit by a harpoon
+    /// Will be called when interacted with by the player
     /// </summary>
-    public void OnHitByHarpoon();
+    abstract void OnInteractedByPlayer();
 }
