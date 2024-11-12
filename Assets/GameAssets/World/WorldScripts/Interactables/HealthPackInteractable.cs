83580163040f644547856cca09238522f34b207f
astapay <astapay@mail.bradley.edu>
2024-11-12 11:39:24 -0600

    Added comments
    Added comments for the HealthPackInteractable script.

--- Assets/GameAssets/World/WorldScripts/Interactables/HealthPackInteractable.cs
+++ Assets/GameAssets/World/WorldScripts/Interactables/HealthPackInteractable.cs
@@ -1,12 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+/*****************************************************************************
+// File Name :         HealthPackInteractable.cs
+// Author :            Andrew Stapay
+// Creation Date :     11/12/24
+//
+// Brief Description : Controls an interactable health pack in scene. When
+                       interacted with, it restores the player's health.
+*****************************************************************************/
 using UnityEngine;
 
 public class HealthPackInteractable : MonoBehaviour, IPlayerInteractable
 {
+    // Variables to store the amount of health restored
+    // and the number of uses before this object is destroyed
     [SerializeField] private int _healthRestored = 20;
     [SerializeField] private int _numUses = 3;
 
+    /// <summary>
+    /// An inherited method that triggers when the object is interacted with.
+    /// Heals the player for a certain amount of health
+    /// </summary>
     public void OnInteractedByPlayer()
     {
         PlayerManager.Instance.InvokePlayerHealEvent(_healthRestored);
@@ -18,6 +30,10 @@ public class HealthPackInteractable : MonoBehaviour, IPlayerInteractable
         }
     }
 
+    /// <summary>
+    /// Called when this health pack is out of uses
+    /// Simply destroys the health pack
+    /// </summary>
     private void OutOfUses()
     {
         Destroy(gameObject);
