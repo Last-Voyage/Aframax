4c27c97ad8c6d5d049a922dadd9a19f50b81ba3d
Alex Kalscheur <akalscheur@mail.bradley.edu>
2024-11-13 10:06:45 -0600

    Remove serialization and related code

--- Assets/General/Scripts/UniversalFunctions/ObjectSpawner.cs
+++ Assets/General/Scripts/UniversalFunctions/ObjectSpawner.cs
@@ -16,36 +16,8 @@ using UnityEngine;
 /// </summary>
 public class ObjectSpawner : MonoBehaviour
 {
-    [Tooltip("The prefab you want to spawn in")]
-    [SerializeField] GameObject _prefab;
-    [Tooltip("The position you want to spawn the object in.  " +
-             "Will be in relation to the parent if defined.  " +
-             "Otherwise will spawn in worldspace")]
-    [SerializeField] Vector3 _position;
-    [Tooltip("The rotation the object will spawn in at" +
-             "Will be in relation to the parent if defined.  " +
-             "Otherwise will spawn in worldspace")]
-    [SerializeField] Quaternion _rotation;
-    [Tooltip("(optional) Object you want the prefab instance childed to")]
-    [SerializeField] Transform _parent;
-    
     /// <summary>
-    /// Default function that uses the defined serialized objects and variables
-    /// </summary>
-    public void SpawnObject()
-    {
-        if(_parent == null)
-        {
-            Instantiate(_prefab, _position, _rotation);
-        }
-        else
-        {
-            Instantiate(_prefab, _position, _rotation, _parent);
-        }        
-    }
-
-    /// <summary>
-    /// Override function for designers' ease.  Used for spawning in worldspace
+    /// Spawns an object based on the given parameters.  Used for spawning in worldspace
     /// </summary>
     /// <param name="prefab">The prefab you want to spawn in</param>
     /// <param name="position">The position in worldspace you want to spawn at</param>
@@ -56,7 +28,7 @@ public class ObjectSpawner : MonoBehaviour
     }
 
     /// <summary>
-    /// Override function for designers' ease.  Used for spawning as a child
+    /// Spawns an object based on the given parameters.  Used for spawning as a child
     /// </summary>
     /// <param name="prefab">The prefab you want to spawn in</param>
     /// <param name="position">The position in worldspace you want to spawn at</param>
