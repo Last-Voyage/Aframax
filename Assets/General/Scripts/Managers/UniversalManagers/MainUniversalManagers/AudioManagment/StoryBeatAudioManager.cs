21ee0119730bd50f0a338195a03ffddac3d9c51c
ZarkosHQ <123423925+ZarkosHQ@users.noreply.github.com>
2024-11-14 15:54:25 -0600

    xml comments and private variable names

--- Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/AudioManagment/StoryBeatAudioManager.cs
+++ Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/AudioManagment/StoryBeatAudioManager.cs
@@ -1,3 +1,11 @@
+/******************************************************************************
+// File Name:       StoryBeatAudioManager
+// Author:          David Henvick
+// Contributors:    
+// Creation Date:   November 14, 2024
+//
+// Description:     Provides the public functions to play sounds
+******************************************************************************/
 using FMOD.Studio;
 using FMODUnity;
 using System.Collections;
@@ -12,14 +20,27 @@ public class AudioManagerTester : MonoBehaviour
     [SerializeField] AudioManager Manager;
     EventInstance _audioEvent;
 
+    /// <summary>
+    /// Used to play a shorter and non-persistant sound
+    /// </summary>
+    /// <param name="index"></param> will be the index of the desired sound
     public void PlayOneshotSound(int index)
     {
         Manager.PlayOneShotSound(OneshotSounds[index]);
     }
+
+    /// <summary>
+    /// Used to play a spersistant sound
+    /// </summary>
+    /// <param name="index"></param> will be the index of the desired sound
     public void PlayAmbient(int index)
     {
         _audioEvent = Manager.PlayAmbientSound(AmbientSounds[index]);
     }
+
+    /// <summary>
+    /// Used to stop the persistant sound
+    /// </summary>
     public void StopAmbient()
     {
         Manager.StopAmbientSound(_audioEvent);
