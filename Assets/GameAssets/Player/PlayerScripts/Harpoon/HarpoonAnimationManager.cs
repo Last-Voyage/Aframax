873111dd27825e6c885b44f3ad1bc20e846f11d5
astapay <astapay@mail.bradley.edu>
2024-11-18 23:08:17 -0600

    Fixed More Looping
    Hooray!

--- Assets/GameAssets/Player/PlayerScripts/Harpoon/HarpoonAnimationManager.cs
+++ Assets/GameAssets/Player/PlayerScripts/Harpoon/HarpoonAnimationManager.cs
@@ -45,52 +45,44 @@ public class HarpoonAnimationManager : MonoBehaviour
         PlayerManager.Instance.GetOnHarpoonRestockCompleteEvent().RemoveListener(ReloadFromEmptyAnimation);
     }
 
-    private IEnumerator DelayedResetTrigger(string anim)
-    {
-        yield return _sleep;
-
-        _animator.ResetTrigger(anim);
-    }
-
     private void StartFocusingAnimation()
     {
-        print("erm, what the sigma");
+        print("focusing");
         _animator.SetTrigger(_FOCUS_START_ANIM);
-        StartCoroutine(DelayedResetTrigger(_FOCUS_START_ANIM));
     }
 
     private void StopFocusingAnimation()
     {
+        print("done focusing");
         _animator.SetTrigger(_FOCUS_END_ANIM);
-        StartCoroutine(DelayedResetTrigger(_FOCUS_END_ANIM));
 
         if (HarpoonGun.Instance.GetHarpoonFiringState() == HarpoonGun.EHarpoonFiringState.Ready)
         {
             _animator.SetTrigger(_DIDNT_SHOOT_ANIM);
-            StartCoroutine(DelayedResetTrigger(_DIDNT_SHOOT_ANIM));
+            print("didnt shoot");
         }
         else
         {
             _animator.SetTrigger(_DID_SHOOT_ANIM);
+            print("did shoot");
 
             if (HarpoonGun.Instance.GetReserveAmmo() > 0)
             {
                 _animator.SetTrigger(_RELOAD_READY_ANIM);
-                StartCoroutine(DelayedResetTrigger(_RELOAD_READY_ANIM));
+                print("reloading");
             }
             else
             {
                 _animator.SetTrigger(_AMMO_EMPTY_ANIM);
+                print("out of ammo");
             }
-
-            StartCoroutine(DelayedResetTrigger(_DID_SHOOT_ANIM));
         }
     }
 
     private void StartFiringAnimation()
     {
         _animator.SetTrigger(_FIRE_ANIM);
-        StartCoroutine(DelayedResetTrigger(_FIRE_ANIM));
+        print("firing");
 
         StopFocusingAnimation();
     }
@@ -99,10 +91,8 @@ public class HarpoonAnimationManager : MonoBehaviour
     {
         if (_animator.GetBool(_AMMO_EMPTY_ANIM))
         {
-            StartCoroutine(DelayedResetTrigger(_AMMO_EMPTY_ANIM));
-
             _animator.SetTrigger(_RELOAD_READY_ANIM);
-            StartCoroutine(DelayedResetTrigger(_RELOAD_READY_ANIM));
+            print("reloading from empty");
         }
     }
 }
