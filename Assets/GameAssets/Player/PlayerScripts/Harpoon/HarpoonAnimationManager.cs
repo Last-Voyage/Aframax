c1e3611f9693dd57c369773f33d6138ab00b96d5
astapay <astapay@mail.bradley.edu>
2024-11-19 14:37:27 -0600

    More Fixes yay!
    More changes to AnimationManager and adjustments to animator

--- Assets/GameAssets/Player/PlayerScripts/Harpoon/HarpoonAnimationManager.cs
+++ Assets/GameAssets/Player/PlayerScripts/Harpoon/HarpoonAnimationManager.cs
@@ -12,11 +12,6 @@ public class HarpoonAnimationManager : MonoBehaviour
     private const string _FOCUS_END_ANIM = "releaseFocus";
     private const string _RELOAD_READY_ANIM = "reloadReady";
     private const string _AMMO_EMPTY_ANIM = "ammoEmpty";
-    private const string _DIDNT_SHOOT_ANIM = "didNotShoot";
-    private const string _DID_SHOOT_ANIM = "didShoot";
-    private const string _RELOADED = "reloaded";
-    
-    // Why is this here? What does looking at a wall change for animations?
     private const string _NOT_WALL_ANIM = "notAtWall";
     private const string _AT_WALL_ANIM = "atWall";
 
@@ -46,44 +41,31 @@ public class HarpoonAnimationManager : MonoBehaviour
         PlayerManager.Instance.GetOnHarpoonFocusEndEvent().RemoveListener(StopFocusingAnimation);
         PlayerManager.Instance.GetOnHarpoonFiredEvent().RemoveListener(StartFiringAnimation);
         PlayerManager.Instance.GetOnHarpoonRestockCompleteEvent().RemoveListener(ReloadFromEmptyAnimation);
-        PlayerManager.Instance.GetOnHarpoonReloadedEvent().RemoveListener(ReloadHarpoonAnimation);
+        PlayerManager.Instance.GetOnHarpoonStartReloadEvent().RemoveListener(ReloadHarpoonAnimation);
     }
 
     private void StartFocusingAnimation()
     {
         _animator.SetTrigger(_FOCUS_START_ANIM);
-        //_animator.ResetTrigger(_FOCUS_START_ANIM);
+        _animator.ResetTrigger(_FOCUS_END_ANIM);
     }
 
     private void StopFocusingAnimation()
     {
-        print("done focusing");
         _animator.SetTrigger(_FOCUS_END_ANIM);
-        //_animator.ResetTrigger(_FOCUS_END_ANIM);
+        _animator.ResetTrigger(_FOCUS_START_ANIM);
 
-        if (HarpoonGun.Instance.GetHarpoonFiringState() == HarpoonGun.EHarpoonFiringState.Ready)
+        if (HarpoonGun.Instance.GetHarpoonFiringState() != HarpoonGun.EHarpoonFiringState.Ready)
         {
-            _animator.SetTrigger(_DIDNT_SHOOT_ANIM);
-            //_animator.ResetTrigger(_DIDNT_SHOOT_ANIM);
-            print("didnt shoot");
-        }
-        else
-        {
-            _animator.SetTrigger(_DID_SHOOT_ANIM);
-            //_animator.ResetTrigger(_DID_SHOOT_ANIM);
-            print("did shoot");
-
             if (HarpoonGun.Instance.GetReserveAmmo() > 0)
             {
                 _animator.SetTrigger(_RELOAD_READY_ANIM);
                 //_animator.ResetTrigger(_RELOAD_READY_ANIM);
-                print("reloading");
             }
             else
             {
                 _animator.SetTrigger(_AMMO_EMPTY_ANIM);
                 //_animator.ResetTrigger(_AMMO_EMPTY_ANIM);
-                print("out of ammo");
             }
         }
     }
@@ -92,20 +74,18 @@ public class HarpoonAnimationManager : MonoBehaviour
     {
         _animator.SetTrigger(_FIRE_ANIM);
         //_animator.ResetTrigger(_FIRE_ANIM);
-        print("firing");
 
         StopFocusingAnimation();
     }
 
     private void ReloadHarpoonAnimation()
     {
-        _animator.SetTrigger(_RELOADED);
+        _animator.SetTrigger(_RELOAD_READY_ANIM);
     }
     
     private void ReloadFromEmptyAnimation(int unusedEventVar)
     {
         _animator.SetTrigger(_RELOAD_READY_ANIM);
         //_animator.ResetTrigger(_RELOAD_READY_ANIM);
-        print("reloading from empty");
     }
 }
