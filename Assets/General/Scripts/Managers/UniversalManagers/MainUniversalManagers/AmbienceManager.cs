160844913fc369321cc00442905b2587c66c65d5
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-11-22 18:30:43 -0600

    Re-added clear ambience function

--- Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/AmbienceManager.cs
+++ Assets/General/Scripts/Managers/UniversalManagers/MainUniversalManagers/AmbienceManager.cs
@@ -121,6 +121,23 @@ public class AmbienceManager : AudioManager
         
         PlayIntervalAudio();
     }
+    
+    /// <summary>
+    /// Stops all ambient audio and clears the list
+    /// </summary>
+    private void ClearAmbientAudio()
+    {
+        if(_allAmbientEvents.Capacity > 0)
+        {
+            // Stop any instances of music playing
+            foreach (var sound in _allAmbientEvents)
+            {
+                sound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            }
+            
+            _allAmbientEvents.Clear();
+        }
+    }
 
     #region Interval Ambience
     
