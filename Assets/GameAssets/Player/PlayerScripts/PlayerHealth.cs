e5560aadfdcf64a04b420d6d41f336068075c99b
Andrea Swihart <100057381+ASLH68@users.noreply.github.com>
2024-11-23 12:17:57 -0600

    Spacing Update PlayerHealth.cs

--- Assets/GameAssets/Player/PlayerScripts/PlayerHealth.cs
+++ Assets/GameAssets/Player/PlayerScripts/PlayerHealth.cs
@@ -97,7 +97,6 @@ public class PlayerHealth : BaseHealth
             PlayerManager.Instance.InvokePlayerDamagedEvent(damage);
             PlayerManager.Instance.InvokePlayerHealthChangeEvent(GetHealthPercent(), _currentHealth);
             
-            
             RuntimeSfxManager.APlayOneShotSfx?
                 .Invoke(FmodSfxEvents.Instance.PlayerTookDamage, gameObject.transform.position);
             PlayHeartBeatSfx();
