ad6a6db62262d98988aecf9cdf3b138c46810030
Nabil Tagba <ntagba@mail.bradley.edu>
2025-01-31 12:33:46 -0600

    sound horror moment
    a prefab the plays a horror moment sound like thump of the night on trigger with it

---
+++ Assets/General/Scripts/HorrorMoments/SoundHorrorMoment.cs
@@ -0,0 +1,34 @@
+/*****************************************************************************
+// File Name :         SoundHorrorMoment.cs
+// Author :            Nabil Tagba
+// Creation Date :     1/31/2024
+//
+// Brief Description : plays a certain sound to scare the player when they come
+//in contact with the trigger
+*****************************************************************************/
+using UnityEngine;
+using FMODUnity;
+using System.Collections.Generic;
+
+public class SoundHorrorMoment : MonoBehaviour
+{
+
+    private bool _hasPlayedHorrorSound = false;
+
+    [SerializeField] private List<EventReference> _horrorMomentSounds;
+    [SerializeField] private int _soundToUseIndex;
+    /// <summary>
+    /// happens when the player comes in contact with
+    /// the collider
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !_hasPlayedHorrorSound) 
+        {
+            //play horror moment souund
+            RuntimeSfxManager.APlayOneShotSfx?.Invoke(_horrorMomentSounds[_soundToUseIndex], transform.position);
+            _hasPlayedHorrorSound = true;
+        }
+    }
+}
