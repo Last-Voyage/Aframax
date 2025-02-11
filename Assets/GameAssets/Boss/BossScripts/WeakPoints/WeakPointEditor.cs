b4122eade08a6ad31a2bc90f5741eb1796f8c69e
MilesRogers3D <130790083+MilesRogers3D@users.noreply.github.com>
2025-02-11 10:45:31 -0600

    Evil fix to include issue

--- Assets/GameAssets/Boss/BossScripts/WeakPoints/WeakPointEditor.cs
+++ Assets/GameAssets/Boss/BossScripts/WeakPoints/WeakPointEditor.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using UnityEditor;
 
+#if UNITY_EDITOR
+
 [CustomEditor(typeof(WeakPointHandler))]
 public class WeakPointEditor : Editor
 {
     public override void OnInspectorGUI()
-    {
+    { 
         base.OnInspectorGUI();
 
         WeakPointHandler weakPointHandler = (WeakPointHandler)target;
 
         if(GUILayout.Button("Spawn weak point test")){   // for some freakin reason the weak point handler script disables itself
             weakPointHandler.SpawnWeakPointEditor();     // in my test scene so I had to make this, although rather useless
-        }                                           
+        }            
     }
 }
+
+#else
+
+// Put here to trick the game into compiling with this script
+public class WeakPointEditor {}
+
+#endif
