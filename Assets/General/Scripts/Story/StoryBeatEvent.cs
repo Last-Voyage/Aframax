3d51c3940c724d39b3bc096a9e150f6eed8c36d0
Charlie Polonus <capolonus@gmail.com>
2025-02-14 11:39:41 -0600

    Wave 3: Declarations
    This just changed some specified variable access levels, as well as moved some classes to new separate scripts

---
+++ Assets/General/Scripts/Story/StoryBeatEvent.cs
@@ -0,0 +1,82 @@
+﻿/*****************************************************************************
+// Name :           StoryBeatEvent.cs
+// Author :         Charlie Polonus
+// Contributer :    Ryan Swanson
+// Created :        11/6/2024
+// Description :    Handles specific events for each story beat
+*****************************************************************************/
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// An individual story beat event
+/// </summary>
+[System.Serializable]
+public class StoryBeatEvent
+{
+    // Enumerable for setting the type of beat event
+    public enum EBeatEventType { Dialogue, BoatSpeed, BossAttack, Function }
+
+    // Editor settings
+    public bool IsMinimized;
+
+    // General settings
+    public string EventName;
+    public EBeatEventType EventType;
+    public float DelayTime;
+
+    // Dialogue settings
+    public ScriptableDialogueUi Dialogue;
+
+    // Boat speed settings
+    public float BoatSpeed;
+    public float SpeedChangeTime;
+
+    // Boss attack settings
+    public List<BaseBossAttack> BossAttacks;
+
+    // Function settings
+    public UnityEvent OnBeatEvent;
+
+    /// <summary>
+    /// Start the story beat event
+    /// </summary>
+    public void TriggerEvent(bool outdated = false)
+    {
+        // Based on the event type, do a specific action
+        switch (EventType)
+        {
+            // If it's a dialogue event, start the dialogue
+            case EBeatEventType.Dialogue:
+                if (!outdated)
+                {
+                    GameStateManager.Instance.GetOnNewDialogueChain()?.Invoke(Dialogue);
+                }
+                break;
+
+            // If it's a boat speed event, change the speed of the boat
+            case EBeatEventType.BoatSpeed:
+                BoatMover.Instance.ChangeSpeed(BoatSpeed, SpeedChangeTime);
+                break;
+
+            // If it's a boss attack event, start the attacks of all the individual attackers
+            case EBeatEventType.BossAttack:
+                foreach (BaseBossAttack currentAttack in BossAttacks)
+                {
+                    currentAttack.InvokeAttackBegin();
+                }
+                break;
+
+            // If it's a UnityEvent event, invoke the UnityEvent
+            case EBeatEventType.Function:
+                OnBeatEvent?.Invoke();
+                break;
+
+            // If it's not a predefined type, send an error line
+            default:
+                Debug.LogError("Unable to read the event type " + EventType.ToString());
+                break;
+        }
+    }
+}
--- Assets/General/Scripts/Story/StoryManager.cs
+++ Assets/General/Scripts/Story/StoryManager.cs
@@ -10,7 +10,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 /// <summary>
 /// The manager for all the story beats and story events
@@ -251,97 +250,5 @@ public class StoryManager : MonoBehaviour
 
         // Tell the active beat list to stop considering this beat as playing
         _beatEventsCoroutines[_activeStoryBeats.IndexOf(beat)] = null;
-    }
-}
-
-/// <summary>
-/// An individual story beat
-/// </summary>
-[System.Serializable]
-public class StoryBeat
-{
-    // The name and description of the story beat
-    public string BeatName;
-    public string BeatDescription;
-
-    // Whether or not the beat should trigger as soon as the game starts
-    public bool TriggerOnStart;
-
-    // Whether or not the beat is outdated
-    public bool Outdated = false;
-
-    // The list of story beat events in the beat
-    public List<StoryBeatEvent> StoryBeatEvents;
-}
-
-/// <summary>
-/// An individual story beat event
-/// </summary>
-[System.Serializable]
-public class StoryBeatEvent
-{
-    // Enumerable for setting the type of beat event
-    public enum EBeatEventType { Dialogue, BoatSpeed, BossAttack, Function }
-
-    // Editor settings
-    public bool IsMinimized;
-
-    // General settings
-    public string EventName;
-    public EBeatEventType EventType;
-    public float DelayTime;
-
-    // Dialogue settings
-    public ScriptableDialogueUi Dialogue;
-
-    // Boat speed settings
-    public float BoatSpeed;
-    public float SpeedChangeTime;
-
-    // Boss attack settings
-    public List<BaseBossAttack> BossAttacks;
-
-    // Function settings
-    public UnityEvent OnBeatEvent;
-
-    /// <summary>
-    /// Start the story beat event
-    /// </summary>
-    public void TriggerEvent(bool outdated = false)
-    {
-        // Based on the event type, do a specific action
-        switch (EventType)
-        {
-            // If it's a dialogue event, start the dialogue
-            case EBeatEventType.Dialogue:
-                if (!outdated)
-                {
-                    GameStateManager.Instance.GetOnNewDialogueChain()?.Invoke(Dialogue);
-                }
-                break;
-
-            // If it's a boat speed event, change the speed of the boat
-            case EBeatEventType.BoatSpeed:
-                BoatMover.Instance.ChangeSpeed(BoatSpeed, SpeedChangeTime);
-                break;
-
-            // If it's a boss attack event, start the attacks of all the individual attackers
-            case EBeatEventType.BossAttack:
-                foreach (BaseBossAttack currentAttack in BossAttacks)
-                {
-                    currentAttack.InvokeAttackBegin();
-                }
-                break;
-
-            // If it's a UnityEvent event, invoke the UnityEvent
-            case EBeatEventType.Function:
-                OnBeatEvent?.Invoke();
-                break;
-
-            // If it's not a predefined type, send an error line
-            default:
-                Debug.LogError("Unable to read the event type " + EventType.ToString());
-                break;
-        }
     }
 }
