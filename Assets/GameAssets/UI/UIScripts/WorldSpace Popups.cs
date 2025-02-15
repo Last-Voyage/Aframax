747ef2880f2777d359820d2831a891a71ee7406a
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-02-15 17:30:02 -0600

    visiblity proximity

--- Assets/GameAssets/UI/UIScripts/WorldSpace Popups.cs
+++ Assets/GameAssets/UI/UIScripts/WorldSpace Popups.cs
@@ -36,6 +36,10 @@ public class WorldSpacePopups : MonoBehaviour
     private float _playerDetectionProximity;
 
     [SerializeField]
+    private float _visibilityProximity;
+    //should be larger than _playerDetectionProximity
+
+    [SerializeField]
     private string _farText;
 
     [SerializeField]
@@ -60,7 +64,12 @@ public class WorldSpacePopups : MonoBehaviour
         if (_playerReference != null)
         {
             //check proximity to player
-            if (Vector3.Distance(_playerReference.transform.position, transform.position) >= _playerDetectionProximity)
+            if (Vector3.Distance(_playerReference.transform.position, transform.position) >= _visibilityProximity)
+            {
+                _objectSpriteReference.sprite = null;
+                _popUpTextContainer.text = null;
+            }
+            else if (Vector3.Distance(_playerReference.transform.position, transform.position) >= _playerDetectionProximity)
             {
                 _objectSpriteReference.sprite = _farDistanceSprite;
                 _popUpTextContainer.text = _farText;
