1ad53df2a96c2ac82faee5c9b7382f083ab4e2ea
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-02-21 02:27:16 -0600

    comments and prefab fix

--- Assets/GameAssets/UI/UIScripts/SceneTransitionsDisableImage.cs
+++ Assets/GameAssets/UI/UIScripts/SceneTransitionsDisableImage.cs
@@ -1,3 +1,11 @@
+/******************************************************************************
+// File Name:       SceneTransitionDisableImage.cs
+// Author:          Jeremiah Peters
+// Creation Date:   February 21, 2025
+//
+// Description:     turns off the scene transition image component when transition is finished
+******************************************************************************/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +13,14 @@ using UnityEngine.UI;
 
 public class SceneTransitionDisableImage : StateMachineBehaviour
 {
-
+    /// <summary>
+    /// disables image component 
+    /// for some reason, an image component with no image defaults to a solid white square
+    /// by disabling the image component, the object can remain enabled and not block buttons
+    /// </summary>
+    /// <param name="animator"></param>
+    /// <param name="stateInfo"></param>
+    /// <param name="layerIndex"></param>
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         SceneTransitionBehaviour.Instance.GetComponent<Image>().enabled = false;
