b9c5e5c8beb4e8d2a710694d43fde11176aa96d8
nrice25 <nrice@mail.bradley.edu>
2025-03-01 21:12:40 -0600

    UI menuing fixes

---
+++ Assets/GameAssets/UI/UIScripts/QuitButtonUITransfer.cs
@@ -0,0 +1,43 @@
+/*****************************************************************************
+// File Name :         PauseMenu.cs
+// Author :            Nick Rice
+// Creation Date :     3/1/25
+//
+// Brief Description : Selects UI buttons when the quit UI closes
+*****************************************************************************/
+
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Selects UI buttons when the quit UI closes
+/// </summary>
+public class QuitButtonUITransfer : MonoBehaviour
+{
+    private Button _firstSelectedButton;
+    
+    private EventSystem _eventSystem;
+
+    [SerializeField]
+    private GameObject _noButton;
+
+    /*private void OnEnable()
+    {
+        _eventSystem = FindObjectOfType<EventSystem>();
+        _eventSystem.SetSelectedGameObject(_noButton);
+    }*/
+
+    /// <summary>
+    /// Selects UI buttons when the quit UI closes
+    /// </summary>
+    private void OnDisable()
+    {
+        _eventSystem = FindObjectOfType<EventSystem>();
+
+        _firstSelectedButton = FindObjectOfType<Button>();
+        
+        _eventSystem.SetSelectedGameObject(_firstSelectedButton.gameObject);
+    }
+}
