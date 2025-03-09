01e175684f90dbdbcfc0c3e12fc6ac800f5f386f
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-03-08 18:26:59 -0600

    rotates on y axis now

--- Assets/GameAssets/UI/UIScripts/WorldSpace Popups.cs
+++ Assets/GameAssets/UI/UIScripts/WorldSpace Popups.cs
@@ -58,7 +58,7 @@ public class WorldSpacePopups : MonoBehaviour
         {
             //rotate to face the player camera
             transform.LookAt(_playerCamera.transform);
-            transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
             transform.Rotate(0, 180, 0);
         }
 
