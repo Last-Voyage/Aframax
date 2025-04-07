87db96eb88261a8f1dbee099358f2e1f742281c7
Ryan Swanson <raswanson@mail.bradley.edu>
2025-04-06 19:43:45 -0500

    GeneralCollision

---
+++ Assets/Scripts/Maze/GeneralPlayerCollisionTrigger.cs
@@ -0,0 +1,34 @@
+/*****************************************************************************
+// File Name :         GeneralPlayerCollisionTrigger.cs
+// Author :            Ryan Swanson
+// Contributor:        
+// Creation Date :     4/6/25
+//
+// Brief Description : Provides a general collision trigger for design
+*****************************************************************************/
+
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Provides a general collision trigger for design to use how they want
+/// </summary>
+public class GeneralPlayerCollisionTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onPlayerCollision;
+    /// <summary>
+    /// Called when contacting the player
+    /// </summary>
+    public void PlayerContact()
+    {
+        _onPlayerCollision?.Invoke();
+    }
+
+    /// <summary>
+    /// Called to remove any listeners
+    /// </summary>
+    private void OnDestroy()
+    {
+        _onPlayerCollision?.RemoveAllListeners();
+    }
+}
