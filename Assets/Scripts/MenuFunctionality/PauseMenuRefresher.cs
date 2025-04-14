7683cf471bca47e11985577e4b957da45bf317b1
SolarFoxG <114117865+SolarFoxG@users.noreply.github.com>
2025-04-14 02:21:04 -0500

    Refresh Upon Close
    Pause menu refreshes to its initial page when it is closed.

---
+++ Assets/Scripts/MenuFunctionality/PauseMenuRefresher.cs
@@ -0,0 +1,25 @@
+/*****************************************************************************
+// File Name :         PauseMenuRefresher.cs
+// Author :            Adam Garwacki
+// Creation Date :     4/14/25
+//
+// Brief Description : Makes the pause menu's state return to default when it
+//                     is closed.
+*****************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuRefresher : MonoBehaviour
+{
+    [SerializeField] private GameObject _basePauseUI;
+    [SerializeField] private GameObject _pauseUIPage2;
+
+    private void OnDisable()
+    {
+        // Ensures pause menu is in default state
+        _pauseUIPage2.SetActive(false);
+        _basePauseUI.SetActive(true);
+    }
+}
