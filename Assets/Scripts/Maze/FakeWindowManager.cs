a10317cafd65418d1e2bfbe662c3493eed26e9d2
MilesRogers3D <130790083+MilesRogers3D@users.noreply.github.com>
2025-04-15 11:29:08 -0500

    In the code, straight "commenting it"

--- Assets/Scripts/Maze/FakeWindowManager.cs
+++ Assets/Scripts/Maze/FakeWindowManager.cs
@@ -1,14 +1,37 @@
-using System;
+/**********************************************************************************************************************
+// File Name :          FakeWindowManager.cs
+// Author :             Miles Rogers
+// Creation Date :      4/15/2025
+//
+// Brief description :  Global singleton that creates the fake window capture container in the current scene
+**********************************************************************************************************************/
+
 using UnityEngine;
 
+/// <summary>
+/// Global singleton that creates the fake window capture container in the current scene
+/// </summary>
 [RequireComponent(typeof(Camera))]
 public class FakeWindowManager : MonoBehaviour
 {
+    /// <summary>
+    /// Global singleton instance
+    /// </summary>
     public static FakeWindowManager Instance { get; private set; }
     
+    /// <summary>
+    /// Prefab for the fake window capture container
+    /// </summary>
     [SerializeField] private GameObject _fakeWindowCaptureContainer;
+    
+    /// <summary>
+    /// Spawn position for the window capture container in the world
+    /// </summary>
     [SerializeField] private Vector3 _worldSpawnPos = new(0.0F, 0.0F, 1500.0F);
     
+    /// <summary>
+    /// Register global singleton, spawn capture container if it does not exist already
+    /// </summary>
     private void Awake()
     {
         // Global static registry
--- Assets/Scripts/Maze/FakeWindowObject.cs
+++ Assets/Scripts/Maze/FakeWindowObject.cs
@@ -1,17 +1,46 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+/**********************************************************************************************************************
+// File Name :          FakeWindowObject.cs
+// Author :             Miles Rogers
+// Creation Date :      4/15/2025
+//
+// Brief description :  Attach this script to objects that need to render fake windows. It ensures the existence
+//                      of the FakeWindowManager context and updates the relevant values in the window mesh's
+//                      material.
+**********************************************************************************************************************/
+
 using UnityEngine;
 
+/// <summary>
+/// Script for objects that need to render fake windows. Ensures the existence of the FakeWindowManager
+/// context and updates the relevant values in the window mesh's material.
+/// </summary>
 public class FakeWindowObject : MonoBehaviour
 {
+    /// <summary>
+    /// Reference to the FakeWindowManager global singleton
+    /// </summary>
     private FakeWindowManager _fakeWindowManager;
+    
+    /// <summary>
+    /// Reference to the player camera's transform value
+    /// </summary>
     private Transform _cameraTransform;
 
+    /// <summary>
+    /// The mesh renderer rendering the fake window texture (assumes the correct
+    /// fake window material is assigned)
+    /// </summary>
     [SerializeField] private MeshRenderer _windowMeshRenderer;
     
+    /// <summary>
+    /// Camera position cached shader property
+    /// </summary>
     private static readonly int _CAMERA_POS = 
         Shader.PropertyToID("_CameraPos");
+    
+    /// <summary>
+    /// Position of this object cached shader property
+    /// </summary>
     private static readonly int _OBJ_POS = 
         Shader.PropertyToID("_ObjPos");
 
@@ -25,6 +54,10 @@ public class FakeWindowObject : MonoBehaviour
         _cameraTransform = _fakeWindowManager.transform;
     }
 
+    /// <summary>
+    /// Update values in fake window material instance on the window mesh
+    /// renderer.
+    /// </summary>
     private void Update()
     {
         _windowMeshRenderer.material.SetVector(
