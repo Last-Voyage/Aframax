93d048671b7a260d4d9ea7f8a61e4c7b38296675
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-04-14 20:50:37 -0500

    added sfx to title screen buttons

---
+++ Assets/Scripts/UI/ButtonSFXManager.cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+// File Name:       ButtonSFXManager.cs
+// Author:          Jeremiah Peters
+// Creation Date:   4/14/2025
+//
+// Description:     Enables menu buttons to play sound effects
+******************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFXManager : MonoBehaviour
+{
+    //the index of sfx to use
+    [SerializeField] private int _clickSFXIndex;
+    public void PlayClickSFX()
+    {
+        RuntimeSfxManager.APlayOneShotSfx?.Invoke(FmodSfxEvents.Instance._buttonUsedSFX[_clickSFXIndex], Vector3.zero);
+    }
+}
