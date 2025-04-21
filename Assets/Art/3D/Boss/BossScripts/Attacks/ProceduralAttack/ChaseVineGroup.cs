e94030f66ffe00dc6435f462c29387525c6dda2d
Charlie Polonus <capolonus@gmail.com>
2025-04-21 18:57:06 -0500

    removing listeners and resetting data

--- Assets/Art/3D/Boss/BossScripts/Attacks/ProceduralAttack/ChaseVineGroup.cs
+++ Assets/Art/3D/Boss/BossScripts/Attacks/ProceduralAttack/ChaseVineGroup.cs
@@ -190,4 +190,20 @@ public class ChaseVineGroup : MonoBehaviour
     public bool IsTriggeredByPlayerWalkThrough() => _isTriggeredByPlayerWalkingBy;
     public bool IsSupposedToKillInstant() => _doeskillPlayerInstantly;
     public float GetPlayerDamageAmount() => _damageToPlayer;
+
+    /// <summary>
+    /// When the script instance is destroyed
+    /// </summary>
+    private void OnDestroy()
+    {
+        PlayerManager.Instance.GetOnPlayerDeath().RemoveListener(StopMovementAudio);
+    }
+
+    /// <summary>
+    /// When the script instance is disabled
+    /// </summary>
+    private void OnDisable()
+    {
+        PlayerManager.Instance.GetOnPlayerDeath().RemoveListener(StopMovementAudio);
+    }
 }
