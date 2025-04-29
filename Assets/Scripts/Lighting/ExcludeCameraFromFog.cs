53f71f3f14a849007b1a41fc639efe14095b5493
MilesRogers3D <130790083+MilesRogers3D@users.noreply.github.com>
2025-04-29 17:56:46 -0500

    I think this fixed it!

---
+++ Assets/Scripts/Lighting/ExcludeCameraFromFog.cs
@@ -0,0 +1,53 @@
+/*****************************************************************************
+// File Name :         ExcludeCameraFromFog.cs
+// Author :            Miles Rogers
+// Creation Date :     4/29/2025
+//
+// Brief Description : Evil trick to exclude a single camera from rendering fog
+*****************************************************************************/
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+/// <summary>
+/// Excludes a camera from rendering fog
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class ExcludeCameraFromFog : MonoBehaviour
+{
+    /// <summary>
+    /// Initial fog setting in the scene the camera exists in
+    /// </summary>
+    private bool _initialFogSetting = true;
+
+    private Camera _thisCamera;
+
+    /// <summary>
+    /// Cache initial fog setting on start
+    /// </summary>
+    private void Start()
+    {
+        _thisCamera = GetComponent<Camera>();
+        _initialFogSetting = RenderSettings.fog;
+        
+        RenderPipelineManager.beginCameraRendering += BeginCameraRendering;
+        RenderPipelineManager.endCameraRendering += EndCameraRendering;
+    }
+
+    private void BeginCameraRendering(ScriptableRenderContext context, Camera renderCamera)
+    {
+        if (renderCamera == _thisCamera)
+        {
+            RenderSettings.fog = false;
+        }
+    }
+
+    private void EndCameraRendering(ScriptableRenderContext context, Camera renderCamera)
+    {
+        if (renderCamera == _thisCamera)
+        {
+            RenderSettings.fog = _initialFogSetting;
+        }
+    }
+}
