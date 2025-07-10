033cb9a26aeecdb080a633c24cbf8ca994c58e1d
Ryan Swanson <raswanson@mail.bradley.edu>
2025-07-10 10:01:35 -0500

    CleaningUpTheStopMusicCode

--- Assets/Scripts/Managers/UniversalManagers/MainUniversalManagers/PersistentAudioManager.cs
+++ Assets/Scripts/Managers/UniversalManagers/MainUniversalManagers/PersistentAudioManager.cs
@@ -361,21 +361,7 @@ public class PersistentAudioManager : AudioManager
     /// </summary>
     public void StopCurrentMusicTrack()
     {
-        // If there is music already trying to fade in, stop doing that
-        if (_musicFadeInCoroutine != null)
-        {
-            StopCoroutine(_musicFadeInCoroutine);
-        }
-        // If there is music already trying to fade out, stop doing that
-        if (_musicFadeOutCoroutine != null)
-        {
-            StopCoroutine(_musicFadeOutCoroutine);
-        }
-        // If there is music already trying to start, stop doing that
-        if(_musicStartCoroutine != null)
-        {
-            StopCoroutine(_musicStartCoroutine); 
-        }
+        StopAllMusicCoroutines();
         
         // Checks if there is any music currently playing
         if (_currentMusicInstance.isValid())
@@ -383,6 +369,30 @@ public class PersistentAudioManager : AudioManager
             // Starts the process of fading the volume to 0
             _musicFadeOutCoroutine =
                 StartCoroutine(ChangePersistentAudioVolume(_currentMusicInstance, 0, true));
+        }
+    }
+
+    /// <summary>
+    /// Stops all coroutines relating to starting and stopping music
+    /// </summary>
+    private void StopAllMusicCoroutines()
+    {
+        // If there is music already trying to fade in, stop doing that
+        if (!_musicFadeInCoroutine.IsUnityNull())
+        {
+            StopCoroutine(_musicFadeInCoroutine);
+        }
+        // Otherwise if there is music already trying to fade out, stop doing that
+        else if (!_musicFadeOutCoroutine.IsUnityNull())
+        {
+            StopCoroutine(_musicFadeOutCoroutine);
+        }
+        
+        // If there is music already trying to start, stop doing that
+        // This should not be an ELSE of the previous statement
+        if(!_musicStartCoroutine.IsUnityNull())
+        {
+            StopCoroutine(_musicStartCoroutine); 
         }
     }
 
@@ -398,21 +408,7 @@ public class PersistentAudioManager : AudioManager
             return;
         }
 
-        // If there is music already trying to fade in, stop doing that
-        if (_musicFadeInCoroutine != null)
-        {
-            StopCoroutine(_musicFadeInCoroutine);
-        }
-        // If there is music already trying to fade out, stop doing that
-        if (_musicFadeOutCoroutine != null)
-        {
-            StopCoroutine(_musicFadeOutCoroutine);
-        }
-        // If there is music already trying to start, stop doing that
-        if(_musicStartCoroutine != null)
-        {
-            StopCoroutine(_musicStartCoroutine); 
-        }
+        StopAllMusicCoroutines();
         
         // Start the actual desired music
         _musicStartCoroutine = StartCoroutine(StartMusicProcess(reference));
