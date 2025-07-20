7e51564b9da3ccd153373a0e236f7f5f50d51a80
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-07-20 03:37:30 -0500

    fixed the gameplay settings bug
    this bug is so weird

--- Assets/Scripts/UI/ButtonSFXManager.cs
+++ Assets/Scripts/UI/ButtonSFXManager.cs
@@ -16,11 +16,28 @@ public class ButtonSFXManager : MonoBehaviour
 {
     //the index of sfx to use
     [SerializeField] private int _clickSFXIndex;
+
+    //used to avoid settings buttons making noise when initializing
+    private float _startTime;
+
     /// <summary>
     /// plays the designated button sfx (currently there is only one)
     /// </summary>
     public void PlayClickSFX()
     {
+        if (_startTime != 0)
+        {
+            RuntimeSfxManager.APlayOneShotSfx?.Invoke(FmodSfxEvents.Instance._buttonUsedSFX[_clickSFXIndex], Vector3.zero);
+        }
+    }
+
+    public void AlwaysPlayClickSFX()
+    {
         RuntimeSfxManager.APlayOneShotSfx?.Invoke(FmodSfxEvents.Instance._buttonUsedSFX[_clickSFXIndex], Vector3.zero);
+    }
+
+    private void OnEnable()
+    {
+        _startTime = Time.time;
     }
 }
